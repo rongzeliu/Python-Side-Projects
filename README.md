4b141fae4793c97452c78a3b958c3fcfad4128e3
crimsoo-n <robinnoodless@gmail.com>
2026-07-15 15:30:40 -0400

    terminate at will

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Summer-basic-side-projects-
+
+Welcome to my summer projects repo! 
+
+This documents some basic projects that I've worked on during the summer before going into university 
+
+Project 1. Python PDF reader 
+-- the program prompts the user to select a pdf file and it reads it out loud from the inputted page number
+-- press CTRL + C to terminate 
--- pypdf.py
+++ pypdf.py
@@ -2,16 +2,30 @@ import pyttsx3
 import PyPDF2
 from tkinter.filedialog import *
 
+# initialize speaker 
+speaker = pyttsx3.init()
+# opens pdf
 book = askopenfilename()
 pdfReader = PyPDF2.PdfReader(book)
 pages = len(pdfReader.pages)
+starting_page = int(input("Start reading on which page?" ))
 
+print("Reading started. Press CTRL + C at any time to stop")
 
+try:
+    for num in range(starting_page, pages):
+        page = pdfReader.pages[num]
+        text = page.extract_text()
 
-for num in range(0, pages):
-    page = pdfReader.pages[num]
-    text = page.extract_text()
-    speaker = pyttsx3.init()
-    speaker.say(text)
-    speaker.runAndWait()
+        # Split text into sentences to make the loop more responsive
+        sentences = text.replace('?', '.').replace('!', '.').split('. ')
+        
+        for sentence in sentences:
+            speaker.say(sentence)
+            speaker.runAndWait()
+except KeyboardInterrupt:
+    print("\n Reading stopped by the user.", flush = True)
+    speaker.stop()
+    
+
        
